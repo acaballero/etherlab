d73f5b6f062d8b6a0db5e665df03fb378ff4fa35
Ángel Caballero Ruiz <angel.caballero.r@gmv.com>
2025-02-03 01:50:46 +0100

    work

---
+++ src/ui/number_edit_view.cpp
@@ -0,0 +1,176 @@
+//
+// Created by Angel Dust on 12/07/2024.
+//
+
+#include "number_edit_view.h"
+#include "Display_afb.h"
+#include "ips_font.h"
+#include "itemsTemplates.hpp"
+#include "ui/button_widget.h"
+#include "ui/widget.h"
+#include "utils.hpp"
+
+bool NumberEditView::on_input(const st_inputEvent event) {
+
+    bool consumed = Widget::on_input(event);
+
+    if (consumed) {
+        return consumed;
+    }
+
+    switch (event.type) {
+        case INPUT_EVENT_TYPE_ENCODER:
+            update_value(value + event.value * step);
+            consumed = true;
+            break;
+        case INPUT_EVENT_TYPE_TOUCH_START:
+
+            consumed = true;
+            break;
+
+        case INPUT_EVENT_TYPE_BUTTON_PRESS:
+        case INPUT_EVENT_TYPE_BUTTON_DBL_PRESS:
+
+            switch (event.value) {
+
+                case FPANEL_DISPLAY_BUTTON_1:
+                    this->on_button(buttons[DECR_BIG]);
+                    break;
+                case FPANEL_DISPLAY_BUTTON_2:
+                    this->on_button(buttons[DECR]);
+                    break;
+                case FPANEL_DISPLAY_BUTTON_3:
+                    this->on_button(buttons[INCR]);
+                    break;
+                case FPANEL_DISPLAY_BUTTON_4:
+                    this->on_button(buttons[INCR_BIG]);
+                    break;
+                case FPANEL_DISPLAY_BUTTON_5:
+                    this->on_button(buttons[OK]);
+                    break;
+                case KEY_BACK:
+                default:
+                case FPANEL_DISPLAY_BUTTON_6:
+                    this->on_button(buttons[CANCEL]);
+                    break;
+            }
+
+            consumed = true;
+            break;
+        default:
+            consumed = false;
+            break;
+    }
+
+    return consumed;
+}
+
+void NumberEditView::init() {
+
+    const auto button_fn = [this](Button &button) { this->on_button(button); };
+
+    title.set_font((FontDef *)&Font_7x10);
+    title.set_aling(ALIGN_CENTER);
+
+    add_child(&title);
+
+    label_widget.set_aling(ALIGN_LEFT);
+    label_widget.set_font((FontDef *)&Font_7x10);
+    label_widget.set_border_radius(false, true, true, false);
+    add_child(&label_widget);
+
+    text_widget.set_font((FontDef *)&Font_11x18);
+
+    text_widget.set_border_radius(true, false, false, true);
+    text_widget.set_aling(ALIGN_RIGHT);
+
+    add_child(&text_widget);
+
+    for (Button &b : buttons) {
+        b.on_select = button_fn;
+        add_child(&b);
+    }
+
+    buttons[CANCEL].on_select = [this](Button &) { this->set_visible(false); };
+
+    display_panel_buttons.set_labels(display_buttons_labels);
+
+    add_child(&display_panel_buttons);
+}
+
+void NumberEditView::on_focus() { buttons[CANCEL].set_focus(true); }
+
+void NumberEditView::update_value(double v) {
+    value = constrain(v, min, max);
+
+    char buff[max_length];
+    format_double(value, buff, decimal_separator, thousand_separator, frac_digits, max_length);
+
+    text_widget.set_label(buff);
+}
+
+void NumberEditView::set_value(double new_value, uint8_t digits, const char *units, const char *label, double min, double max, double step, double step_big) {
+
+    this->frac_digits = digits;
+    this->step = step;
+    this->step_big = step_big;
+    if (!units) {
+        units = "";
+    }
+
+    this->min = min;
+    this->max = max;
+
+    title.set_label(label);
+    update_value(new_value);
+
+    char buf[20];
+    if (units) {
+        snprintf(buf, 20, "%s", units);
+    }
+
+    if (min != max) {
+        char minbuff[10], maxbuff[10];
+        if (min > 1000 || max > 1000) {
+            char eng_min_units[3];
+            char eng_max_units[3];
+            format_eng(minbuff, min, "", eng_min_units, 2, false);
+            format_eng(maxbuff, max, "", eng_max_units, 2, false);
+            snprintf(buf + strlen(buf), 20 - strlen(buf), "\n[%s%s, %s%s]", minbuff, eng_min_units, maxbuff, eng_max_units);
+        } else {
+            snprintf(buf + strlen(buf), 20 - strlen(buf), "\n[%s, %s]", format_double(min, minbuff, '.', ' ', 2, true, 10),
+                     format_double(max, maxbuff, '.', ' ', 2, true, 10));
+        }
+    }
+    label_widget.set_label(buf);
+}
+
+void NumberEditView::on_button(Button &button) {
+
+    switch (button.id) {
+        case INCR_BIG:
+            update_value(value + step_big);
+            break;
+        case INCR:
+            update_value(value + step);
+            break;
+        case DECR:
+            update_value(value - step);
+            break;
+        case DECR_BIG:
+            update_value(value - step_big);
+            break;
+        case CANCEL:
+        case OK:
+
+            if (button.id == OK && on_changed) {
+                on_changed(value);
+            }
+
+            this->set_visible(false);
+
+            break;
+    }
+}
+
+void NumberEditView::before_paint() {}
---
+++ src/ui/number_edit_view.h
@@ -0,0 +1,91 @@
+//
+// Created by Angel Dust on 2/02/2025.
+//
+
+#ifndef TRX_FRONTEND_NUMBER_EDIT_VIEW_H
+#define TRX_FRONTEND_NUMBER_EDIT_VIEW_H
+
+#include "Display_afb.h"
+#include "display_panel_buttons_widget.h"
+#include "stdio.h"
+#include "ui/widget.h"
+#include "view.h"
+#include "button_widget.h"
+#include "math.h"
+#include "label_widget.h"
+#include "main_view.h"
+#include <cstdint>
+
+class NumberEditView : public View {
+  public:
+    static constexpr uint16_t COLS = 4;
+    static constexpr uint16_t MARGIN = 3;
+    static constexpr uint16_t BUTTON_H = 50;
+    static constexpr uint16_t BUTTONS_Y = HEADER_HEIGHT + BUTTON_H + MARGIN * 3;
+    static constexpr uint16_t HEIGHT = BUTTONS_Y + 2 * BUTTON_H + STATUS_HEIGHT;
+    static constexpr uint16_t WIDTH = DISPLAY_X_PIXELS;
+    static constexpr int BUTTON_W = WIDTH / COLS;
+
+    NumberEditView() : View() { init(); }
+
+    NumberEditView(Rect parent_rect) : View(parent_rect) { init(); }
+
+    std::function<void(double)> on_changed{};
+
+    void on_focus() override;
+
+    void init();
+
+    double get_value() const;
+
+    void set_value(double value, uint8_t frac_digits, const char *units, const char *label, double min = 0, double max = 0, double step = 1,
+                   double step_big = 10);
+
+    bool on_input(const st_inputEvent event) override;
+
+    void before_paint() override;
+
+  private:
+    uint16_t focused_button = 0;
+
+    static constexpr char thousand_separator = ' ';
+    static constexpr char decimal_separator = '.';
+    static constexpr uint8_t max_length = 20;
+    enum BUTTONS { DECR_BIG, DECR, INCR, INCR_BIG, OK, CANCEL };
+
+    uint8_t frac_digits = 6;
+
+    double min;
+    double max;
+    double step;
+    double step_big;
+    double value;
+
+    void update_value(double);
+
+    Button buttons[6]{{{0, BUTTONS_Y, BUTTON_W, BUTTON_H}, display, "<<", C565_BLACK, C565_GREY_DARK, BUTTON_STYLE_3D, ALIGN_CENTER, DECR_BIG},
+                      {{BUTTON_W, BUTTONS_Y, BUTTON_W, BUTTON_H}, display, "<", C565_BLACK, C565_GREY_DARK, BUTTON_STYLE_3D, ALIGN_CENTER, DECR},
+                      {{BUTTON_W * 2, BUTTONS_Y, BUTTON_W, BUTTON_H}, display, ">", C565_BLACK, C565_GREY_DARK, BUTTON_STYLE_3D, ALIGN_CENTER, INCR},
+                      {{BUTTON_W * 3, BUTTONS_Y, BUTTON_W, BUTTON_H}, display, ">>", C565_BLACK, C565_GREY_DARK, BUTTON_STYLE_3D, ALIGN_CENTER, INCR_BIG},
+                      {{0, BUTTONS_Y + BUTTON_H, BUTTON_W *COLS / 2, BUTTON_H}, display, "OK", C565_BLACK, C565_GREY_DARK, BUTTON_STYLE_3D, ALIGN_CENTER, OK},
+                      {{COLS / 2 * BUTTON_W, BUTTONS_Y + BUTTON_H, BUTTON_W *COLS / 2, BUTTON_H},
+                       display,
+                       "Cancel",
+                       C565_WHITE,
+                       C565_GREY_DARKER,
+                       BUTTON_STYLE_3D,
+                       ALIGN_CENTER,
+                       CANCEL}};
+
+    Label label_widget{{3 * BUTTON_W, HEADER_HEIGHT + MARGIN * 2, BUTTON_W, BUTTON_H}, C565_GREY_DARKER, C565_GREY_LIGHT, ButtonStyle::BUTTON_STYLE_FLAT};
+    Label text_widget{{0, HEADER_HEIGHT + MARGIN * 2, 3 * BUTTON_W, BUTTON_H}, C565_BLACK, C565_WHITE, ButtonStyle::BUTTON_STYLE_FLAT};
+    Label title{{0, MARGIN, WIDTH, HEADER_HEIGHT}, C565_WHITE, C565_GREY_DARKER, ButtonStyle::BUTTON_STYLE_FLAT};
+
+    const char *display_buttons_labels[6] = {"<<", "<", ">", ">>", "OK", "Cancel"};
+
+    DisplayPanelButtonsWidget display_panel_buttons = {{0, HEIGHT - STATUS_HEIGHT, DISPLAY_X_PIXELS, STATUS_HEIGHT}};
+
+    void on_button(Button &button);
+};
+
+#endif // TRX_FRONTEND_NUMBER_EDITVIEW_H
