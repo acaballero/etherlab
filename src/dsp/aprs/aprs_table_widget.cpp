f482e0c6e4e1030e9de65f4f725c1408bc20653f
Ángel Caballero Ruiz <angel.caballero.r@gmv.com>
2025-06-13 01:02:50 +0200

    work

---
+++ src/dsp/aprs/aprs_table_widget.cpp
@@ -0,0 +1,217 @@
+//
+// Created by Angel Dust on 18/05/2025.
+//
+#include "aprs_table_widget.h"
+#include "Display_afb.h"
+#include "dsp/aprs/aprs_packet.h"
+#include "dsp/aprs/aprs_rx_task.h"
+#include "hw/stm32f4xx/rtc.h"
+#include "input/inputEvent.h"
+#include "ips_font.h"
+#include "status.h"
+#include <cstddef>
+#include <cstring>
+#include <iterator>
+
+namespace dsp_ui {
+
+void APRSTableWidget::paint_callback() {
+
+    char buf[26];
+
+    display->clear();
+
+    display->setFont(this->font);
+    display->set_trim_enabled(false);
+    display->setColor(C565_GREY_DARKER);
+    display->setVerticalLineSpacing(line_spacing);
+    display->drawRoundedRectangle(0, 0, area.box.width, title_height, 2, true);
+    display->gotoXY(4, 3);
+    sprintf(buf, "%-7s %5s %-8s\n", "Source", "Hits", "Time");
+    display->setBgColor(C565_GREY_DARKER);
+    display->setColor(C565_WHITE);
+    display->print(buf);
+
+    display->setBgColor(C565_BLACK);
+
+    for (size_t i = 0; i < sources.size(); i++) {
+        APRSSource *source = &sources[i];
+        int y = title_height + 3 + (i * line_height);
+
+        display->gotoXY(4, y);
+
+        display->setColor(palette16[source->id]);
+        display->setBgColor(C565_TRANSPARENT);
+
+        if (source->id == selected_id) {
+            display->drawRoundedRectangle(0, y - 2, area.box.width, line_height, 2, false);
+        }
+
+        snprintf(buf, sizeof(buf), "%-7s %s%4d %-8s\n", source->source_formatted, source->hits <= 999 ? " " : "+", source->hits <= 999 ? source->hits : 999,
+                 source->time_string);
+
+        display->print(buf);
+
+        if (source->has_position) {
+            // draw map icon
+        }
+    }
+}
+
+void APRSTableWidget::before_paint(){};
+
+bool APRSTableWidget::on_touch(const st_inputEvent e) {
+
+    size_t ix = ((e.point - screen_pos()).y() - title_height) / line_height;
+    if (ix >= 0 && ix < sources.size()) {
+        select(ix);
+    }
+
+    return true;
+}
+
+void APRSTableWidget::select(int ix) {
+    on_select(sources[ix]);
+    selected_id = sources[ix].id;
+    set_dirty();
+}
+
+bool APRSTableWidget::on_input(const st_inputEvent e) {
+
+    bool consumed = Widget::on_input(e);
+
+    if (consumed) {
+        return true;
+    }
+
+    size_t i = 0;
+    switch (e.type) {
+
+        case INPUT_EVENT_TYPE_ENCODER:
+
+            for (i = 0; i < sources.size(); i++) {
+                if (selected_id == sources[i].id) {
+                    break;
+                }
+            }
+
+            if (i == sources.size()) {
+                i = 0;
+            } else {
+                i += e.value > 0 ? 1 : -1;
+            }
+
+            if (i >= 0 && i < sources.size() && sources[i].id != selected_id) {
+                select(i);
+            }
+
+            consumed = true;
+            break;
+
+        default:
+            consumed = false;
+            break;
+    }
+
+    return consumed;
+}
+
+void APRSTableWidget::init() {
+    line_height = font->height + line_spacing;
+    title_height = this->font->height + 5;
+}
+
+int APRSTableWidget::find_free_id() {
+
+    bool used[max_sources];
+    int id = -1;
+
+    for (int i = 0; i < max_sources; i++) {
+        used[i] = false;
+    }
+
+    for (size_t i = 0; i < sources.size(); i++) {
+        APRSSource s = sources[i];
+        if (s.id >= 0) {
+            used[s.id] = true;
+        }
+    }
+
+    for (size_t i = 0; i < sources.size(); i++) {
+        APRSSource s = sources[i];
+        if (s.id == -1) {
+            for (int i = 0; i < max_sources; ++i) {
+                if (!used[i]) {
+                    id = i;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    return id;
+}
+
+int APRSTableWidget::on_packet(dsp::APRSPacket *packet) {
+
+    APRSSource *entry = sources.find([&](const APRSSource &s) {
+        return s.source == packet->get_source();
+    });
+
+    bool isnew = false;
+
+    if (!entry) {
+        entry = sources.push({});
+        entry->source = packet->get_source();
+        isnew = true;
+    }
+
+    st_datetime dt = rtc_get_date_time();
+    uint32_t ts = rtc_to_epoch(&dt.time, &dt.date);
+    entry->age = ts;
+    entry->hits++;
+
+    char buff[20];
+
+    rtc_to_string(dt, true, buff);
+    snprintf(entry->time_string, APRSSource::time_length + 1, "%s", buff);
+
+    packet->get_source_formatted(buff);
+    snprintf(entry->source_formatted, APRSSource::source_length + 1, "%s", buff);
+
+    if (entry->has_position && !packet->has_position()) {
+        // maintain position info
+    } else {
+        //    entry.has_position = packet->has_position();
+        //       entry.pos = packet->get_position();
+    }
+
+    int id = entry->id;
+
+    // Sort by age
+    sources.sort([&](const APRSSource &a, const APRSSource &b) {
+        return a.age > b.age;
+    });
+
+    if (sources.size() > max_sources) {
+        // sources.clear();
+        sources.erase_last(sources.size() - max_sources);
+    }
+
+    // Find it again (not really needed at the moment since we are assigning now() as its timestamp and so it will be the first)
+
+    entry = sources.find([id](const APRSSource &i) {
+        return i.id == id;
+    });
+
+    if (isnew) {
+        entry->id = find_free_id();
+    }
+
+    set_dirty();
+
+    return entry->id;
+}
+
+} // namespace dsp_ui
---
+++ src/dsp/aprs/aprs_table_widget.h
@@ -0,0 +1,76 @@
+//
+// Created by Angel Dust on 18/05/2025.
+//
+
+#ifndef __APRS_TABLE_WIDGET_H__
+#define __APRS_TABLE_WIDGET_H__
+
+#include "Display_afb.h"
+#include "aprs_packet.h"
+#include <functional>
+#include <stdio.h>
+#include "dsp/aprs/aprs_rx_task.h"
+#include "menuBase.h"
+#include "ring_buffer.hpp"
+#include "ui/ui_types.h"
+#include "ui/view.h"
+#include "ui/console_widget.h"
+
+namespace dsp_ui {
+
+struct APRSSource {
+
+    static constexpr uint64_t invalid_key = 0xffffffffffffffff;
+    static constexpr uint8_t source_length = 15;
+    static constexpr uint8_t time_length = 8;
+
+    int id{-1};
+    uint16_t hits{0};
+    uint32_t age{0};
+    uint64_t source{0};
+    char source_formatted[source_length + 1];
+    char time_string[time_length + 1];
+
+    dsp::aprs_pos pos{0, 0, 0, 0};
+    bool has_position = false;
+};
+
+class APRSTableWidget : public Widget {
+  public:
+    APRSTableWidget(Rect parent_rect, int max_rows) : Widget(parent_rect, &lcd) {
+        set_max_rows(max_rows);
+        init();
+    }
+
+    void paint_callback() override;
+    int on_packet(dsp::APRSPacket *packet);
+    bool on_touch(const st_inputEvent) override;
+    bool on_input(const st_inputEvent e) override;
+    void set_max_rows(int n) {
+        assert(n <= MAX_ROWS);
+        max_sources = n;
+    }
+
+    std::function<void(APRSSource &)> on_select = nullptr;
+
+  private:
+    static constexpr int MAX_ROWS = 8;
+
+    RingBuffer<APRSSource, MAX_ROWS> sources;
+
+    uint8_t max_sources = 1;
+    int selected_id{-1};
+    int line_height{0};
+    int title_height{0};
+    int line_spacing{4};
+
+    bool send_updates{false};
+    void before_paint() override;
+    void init();
+    void select(int ix);
+    int find_free_id();
+};
+
+} // namespace dsp_ui
+
+#endif
