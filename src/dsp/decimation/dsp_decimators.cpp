13f5a020ad184e0e978c57dfcab3c788d11dedaa
Ángel Caballero Ruiz <angel.caballero.r@gmv.com>
2025-07-07 01:25:21 +0200

    work

--- src/dsp/decimation/dsp_decimators.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Angel Dust on 21/04/2021.
-//
-
-#include "dsp_iir_decimator.h"
-#include "dsp_fir_decimator_float.h"
-
-DspIIRDecimator<4> IIRDecimator_I{};
-DspIIRDecimator<4> IIRDecimator_Q{};
